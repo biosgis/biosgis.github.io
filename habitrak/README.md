59474252ee5b71048c2a9a67c8dbaf1decf65cca
biosgis <biosgis@gmail.com>
2017-03-30 10:25:25 -0700

    Loaded local contents

---
+++ habitrak/README.md
@@ -0,0 +1,8 @@
+README File for gitlab.com/biosgis/habitrak
+
+Mon, Mar 27, 2017  4:42:10 PM
+
+20170327 Added new .gitlab-ci.yml file to config repo as pages
+
+
+#
---
+++ js/dotty.js
@@ -0,0 +1,72 @@
+//dotty.js 20160523 dchiang 20170309
+var br = "<br />";
+function $(id) {
+    return document.getElementById(id);
+}
+function hide(id) {
+    $(id).style.display = 'none';
+}
+function show(id) {
+    $(id).style.display = 'block';
+}
+function setTitle(s) {
+    this.title = s;
+}
+function toggle(id) {
+    if ($(id).style.display != 'none') {
+        hide(id);
+    } else {
+        show(id);
+    }
+}
+function hid(node) {
+    node.style.display = 'none';
+}
+function shw(node) {
+    node.style.display = '';
+}
+function togl(node) {
+    if (node.style.display != 'none') {
+        hid(node);
+    } else {
+        shw(node);
+    }
+}
+function poplinks() {
+    var links = document.getElementsByTagName("a");//document.links
+    var atotal = 0;
+    for (var i = 0; i < links.length; i++) {
+        var a = links[i];
+        if (a.id.indexOf("goto") === 0) {//FAIL href.indexOf("#") == 0) {
+            a.target = "_self";
+        } else {
+            a.target = "_blank";
+        }
+        atotal = i + 1;
+    }
+    document.getElementsByTagName("h1")[0].title = atotal + " LInks";
+}
+
+function navigatorInfo() {
+    var msg = "navigator.appName=" + navigator.appName + "<br />" +
+        "navigator.cookieEnabled=" + navigator.cookieEnabled + "<br />" +
+        "navigator.platform(operating system)=" + navigator.platform + "<br />" +
+        "navigator.userAgent=" + navigator.userAgent + "<br />";
+    return msg;
+}
+function windowInfo() {
+    var br = "<br />";
+    var msg = "window.innerHeight=" + window.innerHeight + br +
+        "window.innerWidth=" + window.innerWidth + br +
+        "window.scrollX=" + window.scrollX + br + // Horizontal scrolling
+        "window.scrollY=" + window.scrollY + br + // Vertical scrolling
+        "window.screen.availHeight= " + window.screen.availHeight + br +
+        "window.screen.availLeft= " + window.screen.availLeft + br +
+        "window.screen.availTop= " + window.screen.availTop + br +
+        "window.screen.availWidth= " + window.screen.availWidth + br +
+        "window.screen.colorDepth= " + window.screen.colorDepth + br +
+        "window.screen.height= " + window.screen.height + br +
+        "window.screen.pixelDepth= " + window.screen.pixelDepth + br +
+        "window.screen.width= " + window.screen.width;
+    return msg;
+}
