00344abe85207a12346c65ad5ee57367872db763
biosgis <biosgis@gmail.com>
2017-12-08 17:18:32 -0800

    deanbox mod dotty, habitraksde; new arcjshtml;

--- js/dotty.js
+++ js/dotty.js
@@ -1,4 +1,4 @@
-//dotty.js 20160523 dchiang 20170309
+//dotty.js 20160523 dchiang 20171208
 var br = "<br />";
 function $(id) {
     return document.getElementById(id);
@@ -13,12 +13,13 @@ function tagtitle(s) {
     this.title = s;
 }
 function toggle(id) {
-    if ($(id).style.display != 'none') {
+    if ($(id).style.display !== 'none') {
         hide(id);
     } else {
         show(id);
     }
 }
+
 function hid(node) {
     node.style.display = 'none';
 }
@@ -26,49 +27,130 @@ function shw(node) {
     node.style.display = '';
 }
 function togl(node) {
-    if (node.style.display != 'none') {
+    if (node.style.display !== 'none') {
         hid(node);
     } else {
         shw(node);
     }
 }
 function poplinks() {
-    var links = document.getElementsByTagName("a");
+    var links = document.getElementsByTagName('a');
     var atotal = 0;
     for (var i = 0; i < links.length; i++) {
         var a = links[i];
-        if (a.href !== undefined && a.href !== "") {
-            if (a.href.indexOf("#") >= 0 || a.id.indexOf("goto") === 0) {//FAIL href.indexOf("#") == 0) {//
+        if (a.href !== undefined && a.href !== '') {
+            if (a.href.indexOf('#') === 0 || a.id.indexOf('goto') === 0) {
+                //FAIL--a.href.indexOf('#') === 0)
                 a.target = "_self";
-            } else if (a.href.indexOf("mailto:") < 0 && a.href.indexOf("javascript:void") < 0) {
+            } else if ((a.href.indexOf("mailto:") < 0 && a.href.indexOf("javascript:void") < 0)
+                    || a.href.indexOf('http') !== 0) {
                 a.target = "_blank";
+            }
+            if ((a.innerHTML === '' && a.className.indexOf('esri-') < 0) 
+                    || a.className.indexOf('ali') >= 0) {
+                if (a.className === '') {
+                    a.className = 'ali';
+                } else if (a.className.indexOf('ali') < 0) {
+                    a.className = a.className + ' ali';
+                }
+                if (a.title !== '') {
+                    a.innerHTML = '&loz;&nbsp;' + a.title;
+                } else {
+                    a.innerHTML = a.href;
+                }
             }
         }
         atotal = i + 1;
     }
-    console.log(atotal + " LInks")
+    console.log(atotal + ' Links')
     return atotal;
 }
 function navigatorInfo() {
-    var msg = "navigator.appName=" + navigator.appName + "<br />" +
-        "navigator.cookieEnabled=" + navigator.cookieEnabled + "<br />" +
-        "navigator.platform(operating system)=" + navigator.platform + "<br />" +
-        "navigator.userAgent=" + navigator.userAgent + "<br />";
+    var msg = 'navigator.appName=' + navigator.appName + '<br />' +
+        'navigator.cookieEnabled=' + navigator.cookieEnabled + '<br />' +
+        'navigator.platform(operating system)=' + navigator.platform + '<br />' +
+        'navigator.userAgent=' + navigator.userAgent + '<br />';
     return msg;
 }
 function windowInfo() {
-    var br = "<br />";
-    var msg = "window.innerHeight=" + window.innerHeight + br +
-        "window.innerWidth=" + window.innerWidth + br +
-        "window.scrollX=" + window.scrollX + br + // Horizontal scrolling
-        "window.scrollY=" + window.scrollY + br + // Vertical scrolling
-        "window.screen.availHeight= " + window.screen.availHeight + br +
-        "window.screen.availLeft= " + window.screen.availLeft + br +
-        "window.screen.availTop= " + window.screen.availTop + br +
-        "window.screen.availWidth= " + window.screen.availWidth + br +
-        "window.screen.colorDepth= " + window.screen.colorDepth + br +
-        "window.screen.height= " + window.screen.height + br +
-        "window.screen.pixelDepth= " + window.screen.pixelDepth + br +
-        "window.screen.width= " + window.screen.width;
+    var br = '<br />';
+    var msg = 'window.innerHeight=' + window.innerHeight + br +
+        'window.innerWidth=' + window.innerWidth + br +
+        'window.scrollX=' + window.scrollX + br + // Horizontal scrolling
+        'window.scrollY=' + window.scrollY + br + // Vertical scrolling
+        'window.screen.availHeight= ' + window.screen.availHeight + br +
+        'window.screen.availLeft= ' + window.screen.availLeft + br +
+        'window.screen.availTop= ' + window.screen.availTop + br +
+        'window.screen.availWidth= ' + window.screen.availWidth + br +
+        'window.screen.colorDepth= ' + window.screen.colorDepth + br +
+        'window.screen.height= ' + window.screen.height + br +
+        'window.screen.pixelDepth= ' + window.screen.pixelDepth + br +
+        'window.screen.width= ' + window.screen.width;
     return msg;
 }
+function init() {
+    var msg = poplinks();
+    console.log(msg + ' links parsed');
+}
+
+//window.addEventListener('load', init);// turn on in page
+
+function ablinks() {
+    // Auto Build links list items from bare a-tag source code 20171128
+    var k = 0;
+    var libs = document.getElementsByClassName('linkpool');
+    for (var i = 0; i < libs.length; i++) {
+        var lib = libs[i];
+        var links = lib.childNodes;
+        for (var j = 0; j < links.length; j++) {
+            var a = links[j];
+            if (a.innerHTML === '') {
+                if (a.className !== '') {
+                    a.className = a.className + ' ali';
+                } else {
+                    a.className = 'ali';
+                }
+                if (a.title !== '') {
+                    a.innerHTML = '&loz;&nbsp;' + a.title;
+                } else {
+                    a.innerHTML = a.href;
+                }
+                a.target = '_blank';
+                k = k + 1;
+            }
+        }
+    }
+    return k;
+}
+function showall() {
+    var hidden = document.getElementsByClassName('hide');
+    for (var i = 0; i < hidden.length; i++) {
+        var x = hidden[i];
+        x.className.replace('hide', 'show')
+    }
+}
+function hideall() {
+    var shown = document.getElementsByClassName('show');
+    for (var i = 0; i < shown.length; i++) {
+        var x = shown[i];
+        x.className.replace('show', 'hide')
+    }
+}
+
+function showhidden(id) {
+    var hidden = document.getElementsByClassName('hidden');
+    var yes = id.replace('showhidden', '');
+    for (var i = 0; i < hidden.length; i++) {
+        var x = hidden[i];
+        x.style.display = yes;
+    }
+    hide(id);
+    if (id.indexOf('none') < 0) {
+        show(id + 'none');
+    } else {
+        show(id.replace('none', ''));
+    }
+}
+
+//20171206 Created autolink in poplinks instead of ablinks
+
