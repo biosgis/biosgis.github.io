292d28e3faff5482d802e7bbcdb0d1a3fc93d769
biosgis <dean.chiang@wildlife.ca.gov>
2019-11-08 17:24:14 -0800

    deanbox mod

---
+++ js/als.js
@@ -0,0 +1,19 @@
+/* 
+ * als.js 20191108 dfgchiang 
+ * All Layer Sources
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+console.log('Loading als');
+var als = {
+    "EVEG": {
+        id: "EDW_ExistingVegetationRegion05_01",
+        name: "Existing Vegetation",
+        type: "MapServer",
+        tiled: false,
+        urid: "EDW_ExistingVegetationRegion05_01",
+        url: "https://apps.fs.usda.gov/arcx/rest/services/EDW/EDW_ExistingVegetationRegion05_01/MapServer"
+    }
+}
+console.log('Loaded als');
