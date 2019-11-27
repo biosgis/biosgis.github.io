3d484041c48d222500c0bf4864119067f4d37d71
biosgis <biosgis@gmail.com>
2019-11-27 08:41:02 -0800

    deanmac mod

--- js/abide.js
+++ js/abide.js
@@ -2,7 +2,7 @@
 // All Basic App Setup and Universal Functions
 console.log('Loading abide.js');
 //==== GLOBAL VARIABLES
-var avn = 20191125;
+var avn = 20191126;
 var bvn = ((avn - 20000000) / 10000).toFixed(2);
 //avn.toString().subst(2, 2) + '.' + avn.toString().subst(4, 2);
 var fullversion = bvn + '.' + avn.toString().substr(6, 2);
